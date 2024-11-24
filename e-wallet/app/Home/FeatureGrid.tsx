8aabcef00899c37d9c581623b682c4992864987f
olawaleraji <savagedotjsx@gmail.com>
2024-11-24 04:15:28 +0100

    first commit

--- e-wallet/app/Home/FeatureGrid.tsx
+++ e-wallet/app/Home/FeatureGrid.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 import { motion, useAnimation } from 'framer-motion'
 import {
   Shield,
